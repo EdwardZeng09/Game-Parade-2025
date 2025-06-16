95419356085d91b5e0cf7ce417e57118bd6fd937
WhiteDreaming <1944948460@qq.com>
2025-06-16 09:14:20 +0800

    conflict修复

--- GameJam/Assets/EnemyBullet/EnemyBullet.cs
+++ GameJam/Assets/EnemyBullet/EnemyBullet.cs
@@ -2,26 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
     [Header("µ¯Ä»ÊôÐÔ")]
     [SerializeField] public float speed = 10f;
     [SerializeField] public float damage = 10f;
     [SerializeField] public float lifeTime = 2f;
 
-    private Vector2 direction;
 
-    public void SetDirection(Vector2 dir) 
-    {
-    direction = dir.normalized;
-    }
     private void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
-        lifeTime-=Time.deltaTime;
-        if (lifeTime <= 0f) 
+        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+        //transform.Translate(direction * speed * Time.deltaTime);
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0f)
         {
-        Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
