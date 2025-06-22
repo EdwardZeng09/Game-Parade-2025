c81ae7c9a91f1bc0dd307cf677e7b158006837ee
z1ret <1040838840@qq.com>
2025-06-22 18:24:04 +0800

    修改字体数值

--- GameJam/Assets/Code/Menu/StartButton.cs
+++ GameJam/Assets/Code/Menu/StartButton.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class StartButton : MonoBehaviour
 {
     private Button button;
 
+    [SerializeField] private AudioClip btnClick;
+    private AudioSource audioSource;
+
     private void Start()
     {
         button = GetComponent<Button>();
-        button.onClick.AddListener(LoadDemoScene);
+        audioSource = GetComponent<AudioSource>();
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        if (btnClick != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(btnClick);
+            StartCoroutine(WaitAndLoadScene(btnClick.length));
+        }
+        else
+        {
+            LoadDemoScene();
+        }
+    }
+
+    private IEnumerator WaitAndLoadScene(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadDemoScene();
     }
 
     private void LoadDemoScene()
     {
-        SceneManager.LoadScene("BasicGameEnvironment"); 
+        SceneManager.LoadScene("BasicGameEnvironment");
     }
 }
